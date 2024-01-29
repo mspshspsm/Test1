e3d10183a8aaf8f561ca54865eda8ce6fa9d5541
mspshspsm <157770102+mspshspsm@users.noreply.github.com>
2024-01-29 17:21:16 -0500

    Update test-index.js

--- test-index.js
+++ test-index.js
@@ -31,3 +31,40 @@ exports.COVERITY_INSTALL_DIRECTORY_KEY = 'coverity_install_directory';
 exports.COVERITY_POLICY_VIEW_KEY = 'coverity_policy_view';
 exports.COVERITY_REPOSITORY_NAME_KEY = 'coverity_repository_name';
 exports.COVERITY_BRANCH_NAME_KEY = 'coverity_branch_name';
+exports.COVERITY_AUTOMATION_PRCOMMENT_KEY = 'coverity_automation_prcomment';
+exports.COVERITY_PRCOMMENT_ENABLED_KEY = 'coverity_prComment_enabled';
+exports.COVERITY_LOCAL_KEY = 'coverity_local';
+exports.BRIDGE_COVERITY_VERSION_KEY = 'bridge_coverity_version';
+exports.COVERITY_VERSION_KEY = 'coverity_version';
+// Polaris
+/**
+ * @deprecated Use polaris_access_token instead. This can be removed in future release.
+ */
+exports.POLARIS_ACCESSTOKEN_KEY = 'polaris_accessToken';
+exports.POLARIS_ACCESS_TOKEN_KEY = 'polaris_access_token';
+exports.POLARIS_APPLICATION_NAME_KEY = 'polaris_application_name';
+exports.POLARIS_PROJECT_NAME_KEY = 'polaris_project_name';
+exports.POLARIS_ASSESSMENT_TYPES_KEY = 'polaris_assessment_types';
+/**
+ * @deprecated Use polaris_server_url instead. This can be removed in future release.
+ */
+exports.POLARIS_SERVERURL_KEY = 'polaris_serverUrl';
+exports.POLARIS_SERVER_URL_KEY = 'polaris_server_url';
+exports.POLARIS_TRIAGE_KEY = 'polaris_triage';
+exports.POLARIS_PRCOMMENT_ENABLED_KEY = 'polaris_prComment_enabled';
+exports.POLARIS_PRCOMMENT_SEVERITIES_KEY = 'polaris_prComment_severities';
+exports.POLARIS_BRANCH_NAME_KEY = 'polaris_branch_name';
+exports.POLARIS_BRANCH_PARENT_NAME_KEY = 'polaris_branch_parent_name';
+// Blackduck
+exports.BLACKDUCK_URL_KEY = 'blackduck_url';
+exports.BLACKDUCK_API_TOKEN_KEY = 'blackduck_apiToken';
+exports.BLACKDUCK_TOKEN_KEY = 'blackduck_token';
+exports.BLACKDUCK_INSTALL_DIRECTORY_KEY = 'blackduck_install_directory';
+exports.BLACKDUCK_SCAN_FULL_KEY = 'blackduck_scan_full';
+exports.BLACKDUCK_SCAN_FAILURE_SEVERITIES_KEY = 'blackduck_scan_failure_severities';
+exports.BLACKDUCK_AUTOMATION_FIXPR_KEY = 'blackduck_automation_fixpr';
+exports.BLACKDUCK_FIXPR_ENABLED_KEY = 'blackduck_fixpr_enabled';
+exports.BLACKDUCK_FIXPR_MAXCOUNT_KEY = 'blackduck_fixpr_maxCount';
+exports.BLACKDUCK_FIXPR_CREATE_SINGLE_PR_KEY = 'blackduck_fixpr_createSinglePR';
+exports.BLACKDUCK_FIXPR_FILTER_SEVERITIES_KEY = 'blackduck_fixpr_filter_severities';
+exports.BLACKDUCK_FIXPR_USE_UPGRADE_GUIDANCE_KEY = 'blackduck_fixpr_useUpgradeGuidance';
